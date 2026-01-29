6999e640743a8f887ab9b84cf098d8c2a0605579
Alex <romian37@gmail.com>
2026-01-29 20:20:09 +0700

    Initial Commit

---
+++ src/main/java/com/zezame/lipayz/ProjectLipayzApplication.java
@@ -0,0 +1,13 @@
+package com.zezame.lipayz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLipayzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLipayzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zezame/lipayz/ProjectLipayzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zezame.lipayz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLipayzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
