a02fd6004961ea0a7f81a668681c04d0bc172a52
Alex <romian37@gmail.com>
2026-02-01 21:19:24 +0700

    remove history mapper and map manually

--- src/main/java/com/zezame/lipayz/service/impl/HistoryServiceImpl.java
+++ src/main/java/com/zezame/lipayz/service/impl/HistoryServiceImpl.java
@@ -2,7 +2,6 @@ package com.zezame.lipayz.service.impl;
 
 import com.zezame.lipayz.dto.history.HistoryResDTO;
 import com.zezame.lipayz.dto.pagination.PageRes;
-import com.zezame.lipayz.mapper.HistoryMapper;
 import com.zezame.lipayz.mapper.PageMapper;
 import com.zezame.lipayz.model.History;
 import com.zezame.lipayz.repo.HistoryRepo;
@@ -12,19 +11,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RequiredArgsConstructor
 @Service
 public class HistoryServiceImpl implements HistoryService {
     private final HistoryRepo historyRepo;
-    private final HistoryMapper historyMapper;
     private final PageMapper pageMapper;
 
     @Override
     public PageRes<HistoryResDTO> getHistories(Pageable pageable) {
         Page<History> histories = historyRepo.findAll(pageable);
-        return pageMapper.toPageResponse(histories, historyMapper::mapToDto);
+        return pageMapper.toPageResponse(histories, this::mapToDto);
+    }
+
+    private HistoryResDTO mapToDto(History history) {
+        var dto = new HistoryResDTO(history.getId(), history.getTransaction().getCode(),
+                history.getTransactionStatus().getCode());
+        return dto;
     }
 }
