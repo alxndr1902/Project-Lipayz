b2b573405a77f002e2a92d2142d9902533935857
Alexander <romian37@gmail.com>
2026-02-02 17:43:17 +0700

    add custom exception

---
+++ src/main/java/com/zezame/lipayz/exceptiohandler/exception/InvalidNominalException.java
@@ -0,0 +1,7 @@
+package com.zezame.lipayz.exceptiohandler.exception;
+
+public class InvalidNominalException extends RuntimeException {
+    public InvalidNominalException(String message) {
+        super(message);
+    }
+}
