15ff0dfaaaa9928a546013eeb24972ed9e9d10bb
Alexander <romian37@gmail.com>
2026-02-02 17:42:45 +0700

    update data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,37 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-INSERT INTO roles (created_at, created_by, version, code, name)
-SELECT NOW(), NULL, 0, 'SA', 'Super Admin'
+INSERT INTO roles (id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(), NOW(), uuid_generate_v4(), 0, 'SA', 'Super Admin'
     WHERE NOT EXISTS (SELECT 1 FROM roles WHERE code = 'SA');
 
-INSERT INTO roles (created_at, created_by, version, code, name)
-SELECT NOW(), NULL, 0, 'CUST', 'Customer'
+INSERT INTO roles (id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(),NOW(), uuid_generate_v4(), 0, 'CUST', 'Customer'
     WHERE NOT EXISTS (SELECT 1 FROM roles WHERE code = 'CUST');
 
-INSERT INTO roles (created_at, created_by, version, code, name)
-SELECT NOW(), NULL, 0, 'PGA', 'Payment Gateway Admin'
+INSERT INTO roles (id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(), NOW(), uuid_generate_v4(), 0, 'PGA', 'Payment Gateway Admin'
     WHERE NOT EXISTS (SELECT 1 FROM roles WHERE code = 'PGA');
 
+INSERT INTO roles(id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(), NOW(), uuid_generate_v4(), 0, 'SYS', 'System'
+    WHERE NOT EXISTS (SELECT 1 FROM roles WHERE code = 'SYS');
 
-INSERT INTO transaction_status (created_at, created_by, version, code, name)
-SELECT NOW(), NULL, 0, 'SCS', 'Success'
+
+INSERT INTO transaction_status (id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(),NOW(), uuid_generate_v4(), 0, 'SCS', 'Success'
     WHERE NOT EXISTS (SELECT 1 FROM transaction_status WHERE code = 'SCS');
 
-INSERT INTO transaction_status (created_at, created_by, version, code, name)
-SELECT NOW(), NULL, 0, 'RJC', 'Reject'
+INSERT INTO transaction_status (id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(), NOW(), uuid_generate_v4(), 0, 'RJC', 'Reject'
     WHERE NOT EXISTS (SELECT 1 FROM transaction_status WHERE code = 'RJC');
 
-INSERT INTO transaction_status (created_at, created_by, version, code, name)
-SELECT NOW(), NULL, 0, 'PRCS', 'Processing'
+INSERT INTO transaction_status (id, created_at, created_by, version, code, name)
+SELECT uuid_generate_v4(), NOW(), uuid_generate_v4(), 0, 'PRCS', 'Processing'
     WHERE NOT EXISTS (SELECT 1 FROM transaction_status WHERE code = 'PRCS');
 
 
 INSERT INTO users (
+    id,
     created_at,
     created_by,
     version,
@@ -37,8 +42,9 @@ INSERT INTO users (
     is_activated
 )
 SELECT
+    uuid_generate_v4(),
     NOW(),
-    NULL,
+    uuid_generate_v4(),
     0,
     'sa@gmail.com',
     '$2a$12$GljTtavZT7.Z12MYlPWyHuV1dFJ5diadg7.K/ZJ1wo5hpIbKfewZC',
