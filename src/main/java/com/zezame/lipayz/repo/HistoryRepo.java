593613989e19fefc9ad604ea2a3c53e013667ebf
Alexander <romian37@gmail.com>
2026-02-03 10:40:45 +0700

    now history is being shown relevant to their role

--- src/main/java/com/zezame/lipayz/repo/HistoryRepo.java
+++ src/main/java/com/zezame/lipayz/repo/HistoryRepo.java
@@ -1,9 +1,28 @@
 package com.zezame.lipayz.repo;
 
 import com.zezame.lipayz.model.History;
+import com.zezame.lipayz.model.PaymentGateway;
+import com.zezame.lipayz.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface HistoryRepo extends JpaRepository<History, UUID> {
+    @Query("""
+        SELECT h
+        FROM History h
+        WHERE h.transaction.customer = :customer
+        """)
+    Page<History> findByCustomer(@Param("customer") User customer, Pageable pageable);
+
+    @Query("""
+        SELECT h
+        FROM History h
+        WHERE h.transaction.paymentGateway = :paymentGateway
+        """)
+    Page<History> findByPaymentGateway(PaymentGateway paymentGateway, Pageable pageable);
 }
--- src/main/java/com/zezame/lipayz/service/impl/HistoryServiceImpl.java
+++ src/main/java/com/zezame/lipayz/service/impl/HistoryServiceImpl.java
@@ -2,9 +2,13 @@ package com.zezame.lipayz.service.impl;
 
 import com.zezame.lipayz.dto.history.HistoryResDTO;
 import com.zezame.lipayz.dto.pagination.PageRes;
+import com.zezame.lipayz.exceptiohandler.exception.NotFoundException;
 import com.zezame.lipayz.mapper.PageMapper;
 import com.zezame.lipayz.model.History;
 import com.zezame.lipayz.repo.HistoryRepo;
+import com.zezame.lipayz.repo.PaymentGatewayAdminRepo;
+import com.zezame.lipayz.repo.UserRepo;
+import com.zezame.lipayz.service.BaseService;
 import com.zezame.lipayz.service.HistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,19 +17,35 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class HistoryServiceImpl implements HistoryService {
+public class HistoryServiceImpl extends BaseService implements HistoryService {
     private final HistoryRepo historyRepo;
+    private final UserRepo userRepo;
+    private final PaymentGatewayAdminRepo paymentGatewayAdminRepo;
     private final PageMapper pageMapper;
 
     @Override
     public PageRes<HistoryResDTO> getHistories(Pageable pageable) {
-        Page<History> histories = historyRepo.findAll(pageable);
+        var user = getLoginUser(userRepo);
+
+        Page<History> histories = null;
+
+        switch (user.getRole().getCode()) {
+            case "CUST" -> {
+                histories = historyRepo.findByCustomer(user, pageable);
+            }
+            case "PGA" -> {
+                var pga = paymentGatewayAdminRepo.findByUser(user);
+                histories = historyRepo.findByPaymentGateway(pga.getPaymentGateway(), pageable);
+            }
+            case "SA" -> historyRepo.findAll(pageable);
+        }
+
         return pageMapper.toPageResponse(histories, this::mapToDto);
     }
 
     private HistoryResDTO mapToDto(History history) {
         var dto = new HistoryResDTO(history.getId(), history.getTransaction().getCode(),
-                history.getTransactionStatus().getCode());
+                history.getTransactionStatus().getCode(), history.getCreatedAt());
         return dto;
     }
 }
