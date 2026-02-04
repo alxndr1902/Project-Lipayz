d438306758a503d7a8e04d8d020823ab57c64aae
Alexander <romian37@gmail.com>
2026-02-04 17:35:51 +0700

    add custom exception

---
+++ src/main/java/com/zezame/lipayz/exceptiohandler/exception/InvalidParameterException.java
@@ -0,0 +1,7 @@
+package com.zezame.lipayz.exceptiohandler.exception;
+
+public class InvalidParameterException extends RuntimeException {
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+}
