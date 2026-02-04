37ae547027500e53a3f67ad0ae52a287d7cef2d0
Alex <romian37@gmail.com>
2026-02-04 21:49:01 +0700

    remove unused exceptions

--- src/main/java/com/zezame/lipayz/exceptiohandler/exception/InvalidActionException.java
+++
@@ -1,7 +0,0 @@
-package com.zezame.lipayz.exceptiohandler.exception;
-
-public class InvalidActionException extends RuntimeException {
-    public InvalidActionException(String message) {
-        super(message);
-    }
-}
