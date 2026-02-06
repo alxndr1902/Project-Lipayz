823547e8b022e7e85b235067903d6d576973a1d9
Alexander <romian37@gmail.com>
2026-02-06 15:18:05 +0700

    optimize query

--- src/main/java/com/zezame/lipayz/repo/PaymentGatewayAdminRepo.java
+++ src/main/java/com/zezame/lipayz/repo/PaymentGatewayAdminRepo.java
@@ -9,5 +9,11 @@ import java.util.UUID;
 public interface PaymentGatewayAdminRepo extends JpaRepository<PaymentGatewayAdmin, UUID> {
     PaymentGatewayAdmin findByUser(User user);
 
-    PaymentGatewayAdmin findByUser_Id(UUID userId);
+    @Query("""
+        SELECT new com.zezame.lipayz.model.PaymentGatewayAdmin(pg.id)
+        FROM PaymentGatewayAdmin pga
+        JOIN PaymentGateway pg ON pga.paymentGateway.id = pg.id
+        WHERE pga.user.id = :userId
+        """)
+    Optional<PaymentGatewayAdmin> findByUser_Id(UUID userId);
 }
