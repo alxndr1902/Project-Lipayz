5028d97179d047fee0b2d14c2e20d4f95b300089
Alexander <romian37@gmail.com>
2026-02-10 17:16:06 +0700

    stateful refresh token

--- src/main/java/com/zezame/lipayz/controller/AuthController.java
+++ src/main/java/com/zezame/lipayz/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.zezame.lipayz.dto.auth.LoginResponseDTO;
 import com.zezame.lipayz.dto.auth.RefreshTokenRequest;
 import com.zezame.lipayz.exceptiohandler.exception.ForbiddenException;
 import com.zezame.lipayz.exceptiohandler.exception.InvalidRefreshToken;
+import com.zezame.lipayz.model.RefreshToken;
+import com.zezame.lipayz.model.User;
+import com.zezame.lipayz.repo.RefreshTokenRepo;
 import com.zezame.lipayz.service.JwtService;
 import com.zezame.lipayz.service.UserService;
 import com.zezame.lipayz.util.RateLimiterUtil;
@@ -20,7 +23,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -30,6 +34,7 @@ public class AuthController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final RefreshTokenRepo refreshTokenRepo;
 
     private final RateLimiterUtil rateLimiterUtil;
     private final HttpServletRequest httpServletRequest;
@@ -47,35 +52,58 @@ public class AuthController {
         }
         var accessToken = jwtService.generateToken(user.getId().toString(),
                 user.getRole().getCode());
-        var refreshToken = jwtService.generateRefreshToken(user.getId().toString(),
+        var token = jwtService.generateRefreshToken(user.getId().toString(),
                 user.getRole().getCode());
+        saveRefreshToken(user);
 
 //        String key = httpServletRequest.getRemoteAddr() + ":/auth/login";
 //        rateLimiterUtil.reset(key);
 
         return new ResponseEntity<>(new LoginResponseDTO(user.getFullName(),
-                user.getRole().getCode(), accessToken, refreshToken), HttpStatus.OK);
+                user.getRole().getCode(), accessToken, token), HttpStatus.OK);
+    }
+
+    private void saveRefreshToken(User user) {
+        var refreshToken = new RefreshToken();
+        refreshToken.setUser(user);
+
+        var token = jwtService.generateRefreshToken(user.getId().toString(), user.getRole().getCode());
+        refreshToken.setToken(token);
+
+        refreshToken.setExpiryDate(Instant.now().plus(Duration.ofHours(24)));
+
+        refreshTokenRepo.save(refreshToken);
     }
 
     @PostMapping("refresh")
     public ResponseEntity<?> refresh(@RequestBody RefreshTokenRequest request) {
         String refreshToken = request.getRefreshToken();
         var claims = jwtService.validateToken(refreshToken);
-
-        if (!"REFRESH".equals(claims.get("type"))) {
-            throw new InvalidRefreshToken("Invalid Refresh Token");
-        }
-
-        String id = claims.get("id", String.class);
-        String roleCode = claims.get("role", String.class);
-        String newAccessToken = jwtService.generateToken(id, roleCode);
-
-        Map<String, String> tokens = new HashMap<>();
-        tokens.put("accessToken", newAccessToken);
-
-        String key = httpServletRequest.getRemoteAddr() + ":/auth/login";
-        rateLimiterUtil.reset(key);
-
-        return ResponseEntity.ok(tokens);
+        return refreshTokenRepo.findByToken(refreshToken)
+                .map(token -> {
+                    if (jwtService.isTokenExpired(token)) {
+                        refreshTokenRepo.delete(token);
+                        throw new InvalidRefreshToken("Refresh Token Has Expired, Please Login Again");
+                    }
+                    var newToken = jwtService.generateToken(claims.get("id", String.class),
+                            claims.get("role", String.class));
+                    return ResponseEntity.ok(Map.of("accessToken", newToken));
+                })
+                .orElseThrow(() -> new InvalidRefreshToken("Invalid Refresh Token"));
+//        if (!"REFRESH".equals(claims.get("type"))) {
+//            throw new InvalidRefreshToken("Invalid Refresh Token");
+//        }
+//
+//        String id = claims.get("id", String.class);
+//        String roleCode = claims.get("role", String.class);
+//        String newAccessToken = jwtService.generateToken(id, roleCode);
+//
+//        Map<String, String> tokens = new HashMap<>();
+//        tokens.put("accessToken", newAccessToken);
+//
+//        String key = httpServletRequest.getRemoteAddr() + ":/auth/login";
+//        rateLimiterUtil.reset(key);
+//
+//        return ResponseEntity.ok(tokens);
     }
 }
